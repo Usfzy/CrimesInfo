798c1a262ca1657ed12eb09bd700f0010faaa75e
Usfzy <usfzy47@gmail.com>
2023-12-28 18:19:21 +0000

    refactored itemOptionMenu to MenuHost

--- app/src/main/java/com/usfzy/criminalintent/fragment/CrimeListFragment.kt
+++ app/src/main/java/com/usfzy/criminalintent/fragment/CrimeListFragment.kt
@@ -2,8 +2,13 @@ package com.usfzy.criminalintent.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -11,10 +16,14 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.usfzy.criminalintent.R
 import com.usfzy.criminalintent.adapter.CrimeListAdapter
 import com.usfzy.criminalintent.databinding.FragmentCrimeListBinding
+import com.usfzy.criminalintent.model.Crime
 import com.usfzy.criminalintent.viewmodel.CrimeListViewModel
 import kotlinx.coroutines.launch
+import java.util.Date
+import java.util.UUID
 
 class CrimeListFragment : Fragment() {
 
@@ -27,9 +36,7 @@ class CrimeListFragment : Fragment() {
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
@@ -42,17 +49,66 @@ class CrimeListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        (requireActivity() as MenuHost).addMenuProvider(
+            object : MenuProvider {
+
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.fragment_crime_list, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.new_crime -> {
+                            showNewCrime()
+                            true
+                        }
+
+                        else -> false
+                    }
+                }
+
+
+            },
+            viewLifecycleOwner,
+        )
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 crimeListViewModel.crimes.collect { crimes ->
-                    binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes) { crimeId ->
-                        findNavController().navigate(
-                            CrimeListFragmentDirections.showCrimeDetail(crimeId)
-                        )
-                    }
+                    binding.crimeRecyclerView.adapter = CrimeListAdapter(
+                        crimes,
+                        { crimeId ->
+                            findNavController().navigate(
+                                CrimeListFragmentDirections.showCrimeDetail(crimeId)
+                            )
+                        },
+                        { crime ->
+                            viewLifecycleOwner.lifecycleScope.launch {
+                                crimeListViewModel.deleteCrime(crime)
+                            }
+                        },
+                    )
                 }
             }
         }
+    }
+
+    private fun showNewCrime() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title = "",
+                date = Date(),
+                isSolved = false,
+                requiresPolice = false,
+            )
+
+            crimeListViewModel.addCrime(crime)
+            findNavController().navigate(
+                CrimeListFragmentDirections.showCrimeDetail(crime.id)
+            )
+        }
+
     }
 
     override fun onDestroy() {
